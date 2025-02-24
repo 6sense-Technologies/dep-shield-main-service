4ae5c2113b7f7da25b62f93dd01c04dcdce51f40
ahmedfaiyaz-6sense <188864756+ahmedfaiyaz-6sense@users.noreply.github.com>
2025-02-24 19:34:43 +0600

    access control allowed all

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://dep-shield-test.vercel.app'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-    credentials: true, // Allow cookies and authorization headers
-  });
-
   // Use validation pipes
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors();
 
   // Swagger setup
   const config = new DocumentBuilder()
