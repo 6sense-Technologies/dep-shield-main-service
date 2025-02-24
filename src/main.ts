1c2a1d246173ac9fe2ce33a70cdd8dd6a1976330
ahmedfaiyaz-6sense <188864756+ahmedfaiyaz-6sense@users.noreply.github.com>
2025-02-24 18:22:32 +0600

    update cors config

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({
     origin: ['http://localhost:3000', 'https://dep-shield-test.vercel.app'],
-    methods: 'GET,POST,PUT,DELETE',
-    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
   });
   // Swagger setup
   const config = new DocumentBuilder()
@@ -24,6 +24,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(8000);
+  await app.listen(8000, '0.0.0.0');
 }
 bootstrap();
