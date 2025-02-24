384e7652cff91045fbc6382ce71d22a51a5b5c88
ahmedfaiyaz-6sense <188864756+ahmedfaiyaz-6sense@users.noreply.github.com>
2025-02-24 18:32:23 +0600

    updated

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://dep-shield-test.vercel.app'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-  });
+
   // Swagger setup
   const config = new DocumentBuilder()
     .setTitle('NestJS API')
@@ -23,7 +18,11 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://dep-shield-test.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   await app.listen(8000, '0.0.0.0');
 }
 bootstrap();
