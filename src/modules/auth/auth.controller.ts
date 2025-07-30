5dfca6db35671232ac0fc2f8831ffa9ea77691f2
miad-6sense <miad@6sensehq.com>
2025-07-30 14:10:13 +0600

    refactor: reorganize auth.controller.ts for improved readability and add check-env endpoint

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,55 +1,61 @@
-import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { SignupDto, LoginDto, VerifyEmailDto, EmailDTO } from './dto/auth.dto';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { EmailDTO, LoginDto, SignupDto, VerifyEmailDto } from './dto/auth.dto';
 import { AccessTokenGuard } from './guards/accessToken.guard';
 // import { RefreshTokenGuard } from './guards/refreshToken.guard';
 import { RefreshTokenGuard } from './guards/refreshToken.guard';
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService) {}
 
-  @Post('signup')
-  signup(@Body() dto: SignupDto) {
-    return this.authService.signup(dto);
-  }
-  @Post('verify-email')
-  verifyEmail(@Body() verifyEmailDTO: VerifyEmailDto) {
-    return this.authService.verifyToken(verifyEmailDTO);
-  }
+    @Post('signup')
+    signup(@Body() dto: SignupDto) {
+        return this.authService.signup(dto);
+    }
+    @Post('verify-email')
+    verifyEmail(@Body() verifyEmailDTO: VerifyEmailDto) {
+        return this.authService.verifyToken(verifyEmailDTO);
+    }
 
-  @Post('send-otp')
-  sendOTP(@Body() dto: EmailDTO) {
-    return this.authService.sendToken(dto.emailAddress);
-  }
+    @Post('send-otp')
+    sendOTP(@Body() dto: EmailDTO) {
+        return this.authService.sendToken(dto.emailAddress);
+    }
 
-  @Post('login')
-  login(@Body() dto: LoginDto) {
-    return this.authService.login(dto);
-  }
+    @Post('login')
+    login(@Body() dto: LoginDto) {
+        return this.authService.login(dto);
+    }
 
-  @UseGuards(RefreshTokenGuard)
-  @ApiBearerAuth()
-  @Post('refresh')
-  refreshTokens(@Req() req: Request) {
-    const refreshToken: string = req['user'].refreshToken;
+    // check env
+    @Get('check-env')
+    checkEnv() {
+        return process.env;
+    }
 
-    return this.authService.refreshTokens(refreshToken);
-  }
-  // @Post('social-login')
-  // socialLogin(@Body() dto: SocialLoginDto) {
-  //   return this.authService.socialLogin(dto);
-  // }
+    @UseGuards(RefreshTokenGuard)
+    @ApiBearerAuth()
+    @Post('refresh')
+    refreshTokens(@Req() req: Request) {
+        const refreshToken: string = req['user'].refreshToken;
 
-  // @Post('github-login')
-  // githubLogin(@Body() dto: GithubTokenDTO) {
-  //   return this.authService.githubLogin(dto);
-  // }
+        return this.authService.refreshTokens(refreshToken);
+    }
+    // @Post('social-login')
+    // socialLogin(@Body() dto: SocialLoginDto) {
+    //   return this.authService.socialLogin(dto);
+    // }
 
-  @UseGuards(AccessTokenGuard)
-  @ApiBearerAuth()
-  @Get('check-login')
-  checkLogin() {
-    return 'Logged In';
-  }
+    // @Post('github-login')
+    // githubLogin(@Body() dto: GithubTokenDTO) {
+    //   return this.authService.githubLogin(dto);
+    // }
+
+    @UseGuards(AccessTokenGuard)
+    @ApiBearerAuth()
+    @Get('check-login')
+    checkLogin() {
+        return 'Logged In';
+    }
 }
